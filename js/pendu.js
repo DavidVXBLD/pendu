08d2c7ae74fed159458e63b55252ef0bc0f668e6
unknown <david.vieuxbled@outlook.fr>
2021-09-25 20:30:34 +0200

    notWorking

--- js/pendu.js
+++ js/pendu.js
@@ -13,38 +13,80 @@ let possibleWords = [
     "tyran"
 ]
 
-let score = {
-    "player" : 0
-}
-
+let remainingTry = 7;
+let randomWord = randomPick();
+let hiddenWord = hiddenPick();
+let splittedWord = randomWord.split("");
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function interface () {
-    let command = prompt("Veuillez choisir votre destination: tapez j si voulez jouer, r si vous souhaitez consulter les règles, et q si vous désirez quitter le jeu")
-    
-    if (command === "j") {
-        alert("Très bien, commençons à jouer");
-    } 
-      else if (command === "r") {
-        alert("Voici les règles du jeu: \n Vous devez choisir une lettre à chaque tour. \n Si vous vous trompez 7 fois: game over! \n Si vous trouvez le mot avant cela: vous gagnez!");
-        return interface();
-    } 
-      else if (command === "q") {
-        alert("Au revoir!")
-    } 
-      else {
-        alert("Votre saisie est invalide! Veuillez recommencer");
-        return interface();
-    }
+function randomPick() {
+  let randomWord =  possibleWords[Math.floor(Math.random() * possibleWords.length)]
+  return randomWord;
 }
 
-function randomWord() {
-    let randomPick =  wordsArray[Math.floor(Math.random() * wordsArray.length)]
-    
-    let hiddenWord = "";
-      
-    for (i = 0; i < randomPick.length; i++) {
-      hiddenWord = hiddenWord + " _";
-    }
-    return hiddenWord;
+function hiddenPick() {
+  let hiddenWord = [];
+  
+  for (let n = 0; n < randomWord.length; n++) {
+    hiddenWord.push("_");
+  }
+  return hiddenWord;
 }
+
+function interface () {
+  let command = prompt("Une petite partie de Pendu? \nVeuillez choisir votre destination: tapez j si voulez jouer, r si vous souhaitez consulter les règles, et q si vous désirez quitter le jeu")
+    
+  if (command === "j") {
+      alert("Très bien, commençons à jouer");
+      return hangman();
+  } 
+    else if (command === "r") {
+      alert("Voici les règles du jeu: \n Vous devez choisir une lettre à chaque tour. \n Si vous vous trompez 7 fois: hangman over! \n Si vous trouvez le mot avant cela: vous gagnez!");
+      return interface();
+  } 
+    else if (command === "q") {
+      alert("Au revoir!");
+  } 
+    else {
+      alert("Votre saisie est invalide! Veuillez recommencer");
+      return interface();
+  }
+} 
+
+function hangman() {
+  let letter = prompt(`${remainingTry} essais restants. \nChoisissez une lettre. \n ${hiddenWord.join(" ")}`);
+  
+  if (letter.length !== 1) {
+    alert("Votre saisie est invalide! Une vie en moins!");
+    remainingTry --;
+    return hangman();
+  } 
+  else if (letter.length === 1 && remainingTry >= 1 && hiddenWord.indexOf("_") !== -1) {
+    alert("Voyons si il y a un " + letter +" dans notre mot...")
+    
+    for(let n = 0; n < splittedWord.length ; n++) {
+      if (letter === splittedWord[n]) {
+        alert("Bien joué!");
+        hiddenWord.splice(n, 1, letter);
+        return hangman();
+      }   
+      else {
+        alert("Il n'y a pas de " + letter + " dans ce mot, désolé...");
+        remainingTry--;
+        return hangman();
+      }
+    }
+  }
+  else if (remainingTry < 1) {
+    alert(`Tu as epuisé tous tes essais... GAME OVER\nLe mot caché était ${randomWord}`);
+    return interface();
+  }
+  else {
+    alert("Bingo! " + randomWord + " était bien le mot caché! Tu as gagné!");
+    return interface();
+  }
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Start Hangman ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+interface();
