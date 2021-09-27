8ad0dec525a15511b261f57b543c4d79fc6ef4e6
unknown <david.vieuxbled@outlook.fr>
2021-09-27 22:42:57 +0200

    2ndToLastVersion

--- js/pendu.js
+++ js/pendu.js
@@ -37,26 +37,28 @@ function hiddenPick() {
 
 // Initiation du programme: commandes et jeu
 function interface () {
-  let command = prompt("Une petite partie de Pendu? \nVeuillez choisir votre destination: tapez j si voulez jouer, r si vous souhaitez consulter les règles, et q si vous désirez quitter le jeu")
-    
-  if (command === "j") {
-      alert("Très bien, commençons à jouer");
-      remainingTry = 7;
-      randomWord = randomPick();
-      hiddenWord = hiddenPick();
-      splittedWord = randomWord.split("");
-      hangman();
-  } 
+  remainingTry = 7;
+  randomWord = randomPick();
+  hiddenWord = hiddenPick();
+  splittedWord = randomWord.split("");
+  while(remainingTry >= 0 && hiddenWord.indexOf("_") !== -1 && true) {
+    let command = prompt("Une petite partie de Pendu? Veuillez choisir votre destination: \nTapez j si voulez jouer. \nTapez r si vous souhaitez consulter les règles. \nTapez q si vous désirez quitter le jeu.") 
+    if (command === "j") {
+        alert("Très bien, commençons à jouer");
+        
+        hangman();
+    } 
     else if (command === "r") {
       alert("Voici les règles du jeu: \nVous devez choisir une lettre à chaque tour. \nSi vous vous trompez 7 fois: game over! \nSi vous trouvez le mot avant cela: vous gagnez!");
       interface();
-  } 
+    } 
     else if (command === "q") {
-      alert("Au revoir!");
-  } 
+      alert("Fermez la page ou changez d'avis!");
+    } 
     else {
       alert("Votre saisie est invalide! Veuillez recommencer");
       interface();
+    }
   }
 } 
 
@@ -73,25 +75,26 @@ function hangman() {
       alert(`Tu as epuisé tous tes essais... GAME OVER\nLe mot caché était ${randomWord}`);
       interface();
     }
-    let letter = prompt(`${remainingTry} essais restants. \nChoisissez une lettre. \n ${hiddenWord.join(" ")}`);
-    if (letter.length !== 1) {
-      alert("Votre saisie est invalide! Une vie en moins!");
-      remainingTry --; 
-      return hangman();
-    }  
-    alert("Voyons si il y a un " + letter +" dans notre mot...")
-    for(let n = 0; n < splittedWord.length ; n++) {
-      if (letter === splittedWord[n]) {
-        hiddenWord[n] = letter;
-        hasFoundLetter = true;
-      }   
-    } 
-    if (!hasFoundLetter)
-    {
-      alert("Il n'y a pas de " + letter + " dans ce mot, désolé...");
-      remainingTry--; 
-    } else {
-      alert("Bien joué ! Vous avez trouvé une lettre !");
+    else {
+      let letter = prompt(`${remainingTry} essais restants. \nChoisissez une lettre. \n ${hiddenWord.join(" ")}`);
+      if (letter.length !== 1) {
+        alert("Votre saisie est invalide! Une vie en moins!");
+        remainingTry --; 
+        return hangman();
+      }  
+      for(let n = 0; n < splittedWord.length ; n++) {
+        if (letter === splittedWord[n]) {
+          hiddenWord[n] = letter;
+          hasFoundLetter = true;
+        }   
+      } 
+      if (!hasFoundLetter) {
+        alert("Il n'y a pas de " + letter + " dans ce mot, désolé...");
+        remainingTry--; 
+      } 
+      else {
+        alert("Bien joué !");
+      }
     }
   }
 }
